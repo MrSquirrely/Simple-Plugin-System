dd8c3de0b890356a2ef7908d62d0eb7695cc3436
Dagers <daniel@strebinger.at>
2017-01-10 20:40:48 +0100

    Initial commitment

---
+++ src/SimplePluginSystemLibrary/IPlugin.cs
@@ -0,0 +1,20 @@
+﻿namespace SimplePluginSystem.Library
+{
+    /// <summary>
+    /// Represents the plugin interface.
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Represents the initialize method.
+        /// This method will be called when the plugin will be invoked.
+        /// </summary>
+        void Init();
+
+        /// <summary>
+        /// Represents the terminate method.
+        /// This method will be called when the plugin will be terminated.
+        /// </summary>
+        void Terminate();
+    }
+}
